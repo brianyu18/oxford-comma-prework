11603a58804af007ae9d2fd9b66e54d4677ef995
brianyu18 <brianyu1990@gmail.com>
2018-09-24 05:20:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,13 @@
 def oxford_comma(array)
   nu_string = ""
   nu_word = ""
+  # if array.length == 2
+  #   nu_string = 
   array[0..-2].each do |ele|
     nu_word = ele + ", "
     nu_string += nu_word
   end
-  nu_string += array[-1]
+  nu_string += "and " + array[-1]
   nu_string
 end
   
