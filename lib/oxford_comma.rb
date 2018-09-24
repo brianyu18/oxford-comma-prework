08946ce57b57c32faf6a680f0ff4e8d8e2b9af9a
brianyu18 <brianyu1990@gmail.com>
2018-09-24 05:18:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,16 @@ def oxford_comma(array)
   nu_string = ""
   nu_word = ""
   array[0..-2].each do |ele|
-    nu_word
-  
-  array.each do |ele|
     nu_word = ele + ", "
     nu_string += nu_word
   end
+  nu_string += array[-1]
   nu_string
 end
+  
+#   array.each do |ele|
+#     nu_word = ele + ", "
+#     nu_string += nu_word
+#   end
+#   nu_string
+# end
